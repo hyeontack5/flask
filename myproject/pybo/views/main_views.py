2af21486e171003e0e8545a8811b3de6d047d8be
hyeontack5 <sksgusxor@gmail.com>
2021-12-14 22:26:01 +0900

    Refactor: 블루프린트로 기능 분리

--- myproject/pybo/views/main_views.py
+++ myproject/pybo/views/main_views.py
@@ -1,21 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
 
-from pybo.models import Question
+bp = Blueprint('main', __name__, url_prefix='/')
 
-bp = Blueprint("main", __name__, url_prefix="/")
-
-
-@bp.route("/hello")
+@bp.route('/hello')
 def hello_pybo():
-    return "Hello, Pybo!"
+    return 'Hello, Pybo!'
 
-
-@bp.route("/")
+@bp.route('/')
 def index():
-    question_list = Question.query.order_by(Question.create_date.desc())
-    return render_template("question/question_list.html", question_list=question_list)
-
-@bp.route("/detail/<int:question_id>/")
-def detail(question_id):
-    question = Question.query.get_or_404(question_id)
-    return render_template("question/question_detail.html", question=question)
+    return redirect(url_for('question._list'))
